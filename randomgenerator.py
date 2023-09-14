79cd41283b5a0a731b02bfcc70b2391add9c00e3
Your Name <you@example.com>
2023-09-14 23:40:30 +0200

    A program kb 99% keszvan formazas egyeb bugfix szukseges

--- randomgenerator.py
+++ randomgenerator.py
@@ -4,7 +4,7 @@ valasztas = 0
 while valasztas is not int:
     try:
         valasztas = int(input('Szamokat vagy betuket szeretnel generalni(1 szamok 2 betuk): '))
-        if valasztas > 0:
+        if valasztas == 1 or valasztas == 2:
             break
         else:
             print("1v2")
@@ -14,28 +14,33 @@ db = 0
 while db is not int:
     try:
         db = int(input('Hany darab szamot/szoveget szeretnel generalni: '))
-        if valasztas == 1 or valasztas == 2:
+        if db > 0:
             break
         else:
             print("xg")
     except ValueError:
         print('Kerlek szamot irj be')
-
 if valasztas == 1:
     with open("ki.txt", "w") as f:
         for i in range(db):
             x = r.randint(1,20)
             f.write(str(x) + ';')
+alsoertek = 0
+felsoertek = 0
+while alsoertek is not int and felsoertek is not int:
+    try:
+        alsoertek = int(input("Also ertek: "))
+        felsoertek = int(input("Felso ertek: "))
+        if alsoertek and felsoertek > 0 and felsoertek >= alsoertek:
+            break
+        else:
+            print("xg")
+    except ValueError:
+        print('Kerlek szamot irj be')
 if valasztas == 2:
-    alsoertek = int(input("Also ertek: "))
-    felsoertek = int(input("Felso ertek: "))
     with open("ki.txt", "w") as f:
         for i in range(db):
-            y = ""
+            x = ""
             for i in range(r.randint(alsoertek, felsoertek)):
-                x = r.choice(s.ascii_letters)
-                y = y+x
-            f.write(str(y) + ";")
-            print()
-            print()
-            print()
+                x = x+r.choice(s.ascii_letters)
+            f.write(str(x) + ";")
